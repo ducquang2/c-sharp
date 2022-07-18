994bdfb19240653f049bee6cf012940670ed45df
ducquang2 <nducquangg@gmail.com>
2022-07-18 11:46:12 +0700

    basic calculator app

---
+++ test/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace test // Note: actual namespace depends on the project name.
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
